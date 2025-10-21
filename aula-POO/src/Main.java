16163e2482963698b03fffc00bf4b40b7f0d87cd
GUILHERME MARTINS PACHECO <guilherme_pacheco152@estudante.sesisenai.org.br>
2025-10-21 20:41:37 -0300

    aula de POO

---
+++ aula-POO/src/Main.java
@@ -0,0 +1,44 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner caixadeTexto = new Scanner(System.in);
+    int idade;
+    String nome;
+    double altura = 1.74;
+    boolean vivo = true;
+    System.out.println("insira sua idade: ");
+
+    idade = caixadeTexto.nextInt();
+
+    System.out.println("Sua idade é: " + idade);
+
+    caixadeTexto.nextLine();
+
+    System.out.println("Insira seu nome: ");
+
+    nome = caixadeTexto.nextLine();
+
+    System.out.println("Seu nome é: " + nome);
+
+  ArrayList<String> nomes = new ArrayList<>();
+
+  nomes.add("Guilherme");
+  nomes.add("Vinivius");
+  nomes.add("Ricardo");
+
+    System.out.println("nomes são: " + nomes);
+    for (int i = 0; i < nomes.size(); i++) {
+      System.out.println("nome = " + nomes.get(i));
+    }
+
+
+
+  }
+}

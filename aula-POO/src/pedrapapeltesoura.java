3ec6218c689d63c06454c741b47fc4cd50d977ed
Guilherme Pacheco <pachecaddass@gmail.com>
2025-11-02 03:33:34 -0300

    Update de Jokenpô

--- aula-POO/src/pedrapapeltesoura.java
+++ aula-POO/src/pedrapapeltesoura.java
@@ -5,33 +5,79 @@ public class pedrapapeltesoura {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         Random random = new Random();
-
-        System.out.println("Escolha 1- Pedra, 2- Papel, 3- Tesoura");
-        int jogador = entrada.nextInt();
-
+        String gestoJogador = "";
+        String gestoMaquina = "";
+        boolean verifica = false;
         int maquina = random.nextInt(3) + 1;
+        int jogador = 1;
 
-        System.out.println("Você escolheu: " + jogador);
-        System.out.println("A máquina escolheu : " + maquina);
+        while(true) {
+            while(!verifica) {
+                System.out.println("Escolha 1- Pedra, 2- Papel, 3- Tesoura");
+                jogador = entrada.nextInt();
+                entrada.nextLine();
 
-        if (jogador > 3 || maquina > 3){
-            System.out.println("Opção Inválida!");
-            System.exit(0);
+                switch(jogador) {
+                    case 1 -> {
+                        gestoJogador = "Pedra";
+                        verifica = true;
+                    }
+
+                    case 2 -> {
+                        gestoJogador = "Papel";
+                        verifica = true;
+                    }
+
+                    case 3 -> {
+                        gestoJogador = "Tesoura";
+                        verifica = true;
+                    }
+
+                    default -> System.out.println("OPÇÃO INVÁLIDA");
+                }
+            }
+
+            verifica = false;
+
+            while(!verifica) {
+                System.out.println("Escolha 1- Pedra, 2- Papel, 3- Tesoura");
+                switch(maquina) {
+                    case 1 -> {
+                        gestoMaquina = "Pedra";
+                        verifica = true;
+                    }
+
+                    case 2 -> {
+                        gestoMaquina = "Papel";
+                        verifica = true;
+                    }
+
+                    case 3 -> {
+                        gestoMaquina = "Tesoura";
+                        verifica = true;
+                    }
+
+                    default -> System.out.println("OPÇÃO INVÁLIDA");
+                }
+            }
+
+            verifica = false;
+
+            System.out.println("Você escolheu: " + gestoJogador);
+            System.out.println("A máquina escolheu : " + gestoMaquina);
+
+            if (jogador == maquina){
+                System.out.println("Empate! tente novamente");
+
+            } else if ((jogador == 1 && maquina == 3 ||
+                    jogador == 3 && maquina == 2 ||
+                    jogador == 2 && maquina == 1)) {
+                System.out.println("Você venceu!");
+                System.exit(0);
+            } else {
+                System.out.println("Máquina venceu!");
+                System.exit(0);
+            }
         }
-
-        if (jogador == maquina){
-            System.out.println("Empate! tente novamente");
-
-        } else if ((jogador == 1 && maquina == 2 ||
-                jogador == 2 && maquina == 1 ||
-                jogador == 3 && maquina == 2)) {
-            System.out.println("Você venceu!");
-        } else {
-            System.out.println("Máquina venceu!");
-        }
-
-        entrada.close();
-
-
     }
 }
